2dbed2a44602037e2721d0faabb9ec676e683685
zeevana <sromdoni0@gmail.com>
2024-11-21 19:40:44 +0900

    Pesan commit

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -9,7 +9,7 @@ const PaymentPage = () => {
 
   const handlePayment = async () => {
     try {
-      const response = await axios.post("/api/create-transaction", {
+      const response = await axios.post("/api/transaction", {
         totalAmount: amount,
         type: type,
       });
